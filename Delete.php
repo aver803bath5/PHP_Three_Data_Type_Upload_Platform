e3a376390b9a8d2dcd22281cf60c255d5190bce3
aver803bath5 <aver803bath261@gmail.com>
2016-09-23 07:24:49 +0800

    Delete Files Done

--- Delete.php
+++ Delete.php
@@ -1,6 +1,16 @@
 <?php 
-	$id = $_POST[id];
 	require('./connect.php');
+	
+	$id = $_POST[id];
+	$sql = "SELECT file_doc, file_pdf, file_odt FROM files WHERE file_id = $id";
+	$result = mysqli_query($con, $sql);
+	$row = mysqli_fetch_array($result, MYSQLI_NUM);
+	for ($i=0; $i < 3; $i++) { 
+		if (file_exists($row[$i])) {
+			unlink($row[$i]);
+		}
+	}
 	$sql = "DELETE FROM files WHERE file_id = $id";
 	mysqli_query($con, $sql);
+
  ?>
